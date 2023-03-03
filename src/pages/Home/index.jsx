6c527e32896cba502623e8140e46d1ee0e7e90d0
unknown <bruno_correa_ds@hotmail.com>
2023-03-03 20:20:43 -0300

    atualizações

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,37 +1,38 @@
 import '../Home/styles.css'
-import Card from '../../components/Card'
 import React,{useState, useEffect} from 'react';
-
+import gif from '../../assets/engrenagem.gif'
 function Home() {
-const [studantName,setStudantName] = useState();
-const [studants, setStudant] = useState([]);
-const [user,setUser] = useState();
-function handleAddStudant(){
-  const newStudant = {
-    city: studantName,
-  }
-  setStudant(( )=>[newStudant])
-}
-
-useEffect(() => {
-  fetch('https://api.bigdatacloud.net/data/reverse-geocode-client')
-  .then(response => response.json())
-  .then(data => {
-    setUser({
-      city: data.city,
-    })
-  })
-},[])
 return(
   <div className='container'>
-     <h1>Busque sua cidade</h1>
-  <button onClick={handleAddStudant} type="button">Buscar</button>
+    <div className='imagem'>
+     <h2 class='textoImg'>
+      Quer ter resultados acima da média? Junte-se com quem está acima da média!
+       </h2>
+    <h1 class='textoImgCaps'>
+    Aumente em até 6x o seu ROI!
+    </h1>
+    <h2 class='textoImg'>
+    Descubra agora mesmo qual gargalo na sua empresa te impede de ter resultados como esses!
+    <button className='buttonInitial'>Quero faturar mais</button>
+       </h2>                                   
+    </div>
 
-  {
-    studants.map(studant =>  <Card key={studant.time} name={studant.city} />)
-   }
+    <h2 class='textSecond'>
+    Resolvemos problemas reais da sua empresa.
+    </h2>
+    <h1 class='textSecond'>O simples que funciona!</h1>
+    <h4 class='textSecond'>Somos uma agência full service, o básico de toda agência, claro que fazemos…</h4>
+    <h3 class='textSecond'>Mas falando sério, de empresário para empresário(a), vamos falar do que importa! $$$</h3>
+    <div className='divImg' >   
+      <img src={gif} alt="loading..."/>
+      <img src={gif} alt="loading..."/>
+      <img src={gif} alt="loading..."/>
+      <p>teste</p>
+      </div>
+    
+    
 
   </div>
 )
 }
-export default Home
+export default Home 
